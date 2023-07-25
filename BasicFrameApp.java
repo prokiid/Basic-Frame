dd1ef53777ce00adbf49dcf47e5a0cb2f0f3d9a3
prokiid <75542268+prokiid@users.noreply.github.com>
2023-07-25 22:37:50 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,17 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+public class BasicFrameApp {
+
+   
+    public static void main(String[] args) {
+       
+        //Declare Variable
+        MyFirstFrame myFrame;
+        
+        myFrame = new MyFirstFrame();
+        
+    }
+    
+}
